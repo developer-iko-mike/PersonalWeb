14ff2f383fbaf5d123286517343d725cc5d379fe
mohammadiko <developer.iko.mike@gmail.com>
2025-06-26 21:18:31 -0700

    finish services but have a bug

---
+++ src/Pages/home/components/services/Service.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "./services.css";
+
+const Service = ({ title, icon, caption, classNames = "cl-4" }) => {
+  const [svgFill, setSvgFill] = useState("03ACF8");
+  const [boxShdowColor, setBoxShadowColor] = useState("rgba(19,182,255,35%)");
+
+  useEffect(() => {
+    if (classNames.includes("service_webDesign")) {
+      setSvgFill("#803AFB");
+      setBoxShadowColor("#803AFB");
+    } else if (classNames.includes("service_interfaceDesign")) {
+      setSvgFill("#FFBB01");
+      setBoxShadowColor("#FFBB01");
+    } else if (classNames.includes("service_webDevelopment")) {
+      setSvgFill("#F5276C");
+      setBoxShadowColor("#F5276C");
+    } else if (classNames.includes("service_uXDesign")) {
+      setSvgFill("#FF5823");
+      setBoxShadowColor("rgba(255,99,49,35%)");
+    } else if (classNames.includes("service_sEOOptimize")) {
+      setSvgFill("#49B846");
+      setBoxShadowColor("rgba(73,175,70,35%)");
+    } else {
+      setSvgFill("#03ACF8");
+      setBoxShadowColor("rgba(19,182,255,35%)");
+    }
+  }, [svgFill]);
+
+  return (
+    <div
+      className={`service pr z100 ${classNames}`}
+      style={{ boxShadow: `0 0 4rem ${boxShdowColor}` }}
+    >
+      {icon}
+      <h3 className="service_title cwhite fw700">{title}</h3>
+      <p className="service_caption cwhite tc">{caption}</p>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="100%"
+        height="223.59"
+        viewBox="0 0 369.92 223.59"
+        className="service_icon pa b0 l0 r0 z-1"
+      >
+        <path
+          id="shape"
+          d="M1249.08,2700.557s15.007-66.794,51.8-54.34,43.02,57.737,121.7,13.585,95.662-58.3,124.531-23.774S1619,2601.5,1619,2601.5v223.59H1249.08Z"
+          transform="translate(-1249.08 -2601.498)"
+          style={{width: "100% !important"}}
+          fill={svgFill}
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default Service;
--- src/Pages/home/components/services/Services.jsx
+++ src/Pages/home/components/services/Services.jsx
@@ -1,12 +1,85 @@
-import React from 'react'
-import "./services.css"
+import React, { useState } from "react";
+import SectionHeading from "../../../../AllRoute_Components/sectionHeading/SectionHeading";
+import WebDesignSVG from "./serviceSVG/WebDesignSVG";
+import InterfaceSVG from "./serviceSVG/InterfaceSVG";
+import DevelopmentSVG from "./serviceSVG/DevelopmentSVG";
+import UxDesignSVG from "./serviceSVG/UxDesignSVG";
+import MotionSVG from "./serviceSVG/MotionSVG";
+import SeoSVG from "./serviceSVG/SeoSVG";
+import Service from "./Service";
+import "./services.css";
 
 const Services = () => {
+  const [serviceItem, setServiceItem] = useState([
+    {
+      id: 1,
+      title: "Web Design",
+      icon: <WebDesignSVG />,
+      caption:
+        "Lipsum as it is sometimes known is dummy text used in laying out print, graphic or web designs.",
+      color: "service_webDesign",
+    },
+    {
+      id: 2,
+      title: "Interface Design",
+      icon: <InterfaceSVG />,
+      caption:
+        "Lipsum as it is sometimes known is dummy text used in laying out print, graphic or web designs.",
+      color: "service_interfaceDesign",
+    },
+    {
+      id: 3,
+      title: "Web Development",
+      icon: <DevelopmentSVG />,
+      caption:
+        "Lipsum as it is sometimes known is dummy text used in laying out print, graphic or web designs.",
+      color: "service_webDevelopment",
+    },
+    {
+      id: 4,
+      title: "UX Design",
+      icon: <UxDesignSVG />,
+      caption:
+        "Lipsum as it is sometimes known is dummy text used in laying out print, graphic or web designs.",
+      color: "service_uXDesign",
+    },
+    {
+      id: 5,
+      title: "Motion Graphic",
+      icon: <MotionSVG />,
+      caption:
+        "Lipsum as it is sometimes known is dummy text used in laying out print, graphic or web designs.",
+      color: "service_motionGraphic",
+    },
+    {
+      id: 6,
+      title: "SEO Optimize",
+      icon: <SeoSVG />,
+      caption:
+        "Lipsum as it is sometimes known is dummy text used in laying out print, graphic or web designs.",
+      color: "service_sEOOptimize",
+    },
+  ]);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id="services">
+      <SectionHeading title={"services"} caption={"what we provide you ?"} />
+      <div className="container ptb5">
+        <div className="rw services__warraper">
+          {serviceItem.map((item) => (
+            <div key={item.id} className="cl-4">
+            <Service
+              title={item.title}
+              icon={item.icon}
+              caption={item.caption}
+              classNames={`dfcjac g2-5 br-5 w100 ${item.color}`}
+              />
+              </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Services;
